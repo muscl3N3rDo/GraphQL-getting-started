870f1baaa099b326a323a1d354ffbe917d385f6e
The Muscl3 <muscl3n3rd@outlook.com>
2018-12-03 16:18:26 -0300

    adding express and express-graphql server

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const { graphql, buildSchema } = require('graphql');
+
+const PORT = process.env.PORT || 3000;
+
+const server = express();
 
 const schema = buildSchema(`
   type Video {
@@ -11,12 +17,28 @@ const schema = buildSchema(`
   },
   type Query {
     video: Video
+    videos: [Video]
   }
   type Schema{
     query: Query
   }
 `);
 
+const videoA = {
+  id: 'a',
+  title: 'Create a GraphQL Schema',
+  duration: 120,
+  watched: true,
+};
+
+const videoB = {
+  id: 'b',
+  title: 'Ember.Js CLI',
+  duration: 240,
+  watched: false,
+};
+
+const videos = [videoA, videoB];
 
 const resolvers = {
   video: () => ({
@@ -25,19 +47,16 @@ const resolvers = {
     duration: 180,
     watched: true
   }),
+  videos: () => videos,
 };
 
-const query = `
-query myFirstQuery {
-  video{
-    id,
-    title,
-    duration,
-    watched
-  }
-}
-`;
 
-graphql(schema, query, resolvers)
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error));
+server.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+  rootValue: resolvers,
+}));
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+})
