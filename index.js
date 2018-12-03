bcd9e0705d471d743342d9be47cefca7ce5f5e05
The Muscl3 <muscl3n3rd@outlook.com>
2018-12-03 17:11:14 -0300

    adding data and nonnull object config in video query

--- index.js
+++ index.js
@@ -2,59 +2,73 @@
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { graphql, buildSchema } = require('graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLBoolean,
+ } = require('graphql');
+
+const {getVideoById} = require('./src/data');
 
 const PORT = process.env.PORT || 3000;
 
 const server = express();
 
-const schema = buildSchema(`
-  type Video {
-    id: ID,
-    title: String,
-    duration: Int,
-    watched: Boolean
+const videoType = new GraphQLObjectType({
+  name: 'Video',
+  description: 'A video',
+  fields: {
+    id: {
+        type: GraphQLID,
+        description: 'The id of the video.'
+      },
+      title: {
+        type: GraphQLString,
+        description: 'The title of the video.',
+      },
+      duration: {
+        type: GraphQLInt,
+        description: 'The duration of the video (in seconds).',
+      },
+      watched: {
+        type: GraphQLBoolean,
+        description: 'Whether or not the viewer has watched the video.'
+      },
   },
-  type Query {
-    video: Video
-    videos: [Video]
+});
+
+const queryType = new GraphQLObjectType({
+  name: 'QueryType',
+  description: 'The Rot query type',
+  fields: {
+    video: {
+      type: videoType,
+      args: {
+        id:{
+          type: new GraphQLNonNull(GraphQLID),
+          description: 'the id of the video'
+        },
+      },
+      resolve: (_, args) => {
+        return getVideoById(args.id);
+      }
+    }
   }
-  type Schema{
-    query: Query
-  }
-`);
+});
 
-const videoA = {
-  id: 'a',
-  title: 'Create a GraphQL Schema',
-  duration: 120,
-  watched: true,
-};
+const schema = new GraphQLSchema({
+  query: queryType
+})
 
-const videoB = {
-  id: 'b',
-  title: 'Ember.Js CLI',
-  duration: 240,
-  watched: false,
-};
-
-const videos = [videoA, videoB];
-
-const resolvers = {
-  video: () => ({
-    id: '1',
-    title: 'bar',
-    duration: 180,
-    watched: true
-  }),
-  videos: () => videos,
-};
 
 
 server.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
-  rootValue: resolvers,
 }));
 
 server.listen(PORT, () => {
